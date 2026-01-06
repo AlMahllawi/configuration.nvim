4da959280cf21ac23dcb7bbe656284d41a0692ce
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2026-01-06 21:11:17 +0200

    Insert lines & disable arrows

--- init.lua
+++ init.lua
@@ -189,11 +189,15 @@ end, { desc = 'Open [T]erminal' })
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Insert lines without entering insert mode
+vim.keymap.set('n', '<leader>o', ':<C-u>put =repeat(nr2char(10), v:count1)<CR>', { desc = 'Put blank line below' })
+vim.keymap.set('n', '<leader>O', ':<C-u>put! =repeat(nr2char(10), v:count1)<CR>', { desc = 'Put blank line above' })
+
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
